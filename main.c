4616eb49c4f9a170ead8757c29626bdb956a884f
joaovml20 <joaolima15257@gmail.com>
2026-06-13 12:37:45 -0300

    atualizacao, adicionando mais 2 operacoes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@ int main(){
     scanf("%lf",&numero1);
     printf("agora digite o segundo numero: ");
     scanf("%lf",&numero2);
-    printf("agora digite a operacao: 1 para somar, 2 para subtracao: ");
+    printf("agora digite a operacao: 1 para somar, 2 para subtracao, 3 para multiplicar, 4 para dividir: ");
     scanf("%d",&resposta);
     switch(resposta){
         case 1:
@@ -14,6 +14,16 @@ int main(){
             break;
         case 2:
             printf("resultado da operacao: %.2lf - %.2lf = %.2lf",numero1,numero2,numero1 - numero2);
+            break;
+        case 3:
+            printf("resultado da operacacao: %.2lf * %.2lf = %.2lf",numero1,numero2,numero1 * numero2);
+            break;
+        case 4: 
+            if(numero2 == 0){
+                printf("impossivel dividir por zero");
+            } else {
+                printf("resultado da operacao: %.2lf / %.2lf = %.2lf",numero1,numero2,numero1 / numero2);
+            }
             break;
         default:
             printf("operacao não encontradar");

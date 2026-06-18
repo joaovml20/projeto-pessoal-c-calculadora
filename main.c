392e67e0933147a073901f074457828ca79a204a
joaovml20 <joaolima15257@gmail.com>
2026-06-18 20:44:13 -0300

    adição de mais 2 operações e biblioteca math.h

--- main.c
+++ main.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
     int resposta;
     double numero1,numero2;
     do{
-        printf("bem-vindo a calculadora, digite a operacao a ser realizada: 0 para fechar a calculadora, 1 para soma, 2 para subtrair, 3 para multiplicar, 4 para dividir: ");
+        printf("bem-vindo a calculadora, digite a operacao a ser realizada:\n 0 para fechar a calculadora \n 1 para soma \n 2 para subtrai \n 3 para multiplicar \n 4 para dividir \n 5 para calcular a raiz quadrada \n 6 para calcular uma potencia:\n");
         scanf("%d",&resposta);
         switch(resposta){
             case 0:
@@ -42,6 +43,22 @@ int main(){
                 } else {
                     printf("resultado da operacao: %.2lf / %.2lf = %.2lf",numero1,numero2,numero1 / numero2);
                 }
+                break;
+            case 5:
+                printf("informe o numero para descobrir sua raiz quadrada: ");
+                scanf("%lf",&numero1);
+                while(numero1 < 0){
+                    printf("impossivel calcular a raiz quadrada de numero negativo, tente novamente com outro numero: ");
+                    scanf("%lf",&numero1);
+                }
+                printf("resultado da operacao: √%.2lf = %.2lf",numero1,sqrt(numero1));
+                break;
+            case 6:
+                printf("informe o valor do primeiro numero: ");
+                scanf("%lf",&numero1);
+                printf("agora informe o valor do segundo numero: ");
+                scanf("%lf",&numero2);
+                printf("resultado da operacao: %.2lf ^ %.2lf = %.2lf",numero1,numero2,pow(numero1,numero2));
                 break;
             default:
                 printf("operacao não encontradar");
